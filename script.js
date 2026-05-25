70681805d14f639db8f1dbeda73b6957c56ae537
pushpa1011 <105695742+pushpa1011@users.noreply.github.com>
2026-05-25 11:51:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-function firstWord(s) {
-  // your code here
+function firstWord(str) {
+    // Trim leading spaces
+    str = str.trim();
+
+    // If string is empty, return empty string
+    if (str === "") return "";
+
+    // Find first space
+    let index = str.indexOf(" ");
+
+    // If no space found, return entire string
+    if (index === -1) return str;
+
+    // Return substring till first space
+    return str.substring(0, index);
 }
 
 // Do not change the code below
